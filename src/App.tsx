973052537325e8f5ffc911f7c63fc21e0354f846
Sirisha Medicharla <sirishamedicharla@Sirishas-MacBook-Pro.local>
2025-03-16 19:26:47 -0700

    updated website content-v4

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,21 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+ // useEffect(() => {
+  //  client.models.Todo.observeQuery().subscribe({
+  //    next: (data) => setTodos([...data.items]),
+ //   });
+ // }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+ // function createTodo() {
+ //   client.models.Todo.create({ content: window.prompt("Todo content") });
+ // }
 
   return (
     <main>
       <h1>Helloo.. More interesting contents would be uploaded to this personal blog soon.</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-            <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
     </main>
   );
 }
