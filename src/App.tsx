f4376c69783403407cd0732dc86a6ceaeae329ae
Sirisha Medicharla <sirishamedicharla@Sirishas-MacBook-Pro.local>
2025-03-16 19:42:39 -0700

    updated website content-v5

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,10 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
- // useEffect(() => {
-  //  client.models.Todo.observeQuery().subscribe({
-  //    next: (data) => setTodos([...data.items]),
- //   });
- // }, []);
-
- // function createTodo() {
- //   client.models.Todo.create({ content: window.prompt("Todo content") });
- // }
-
   return (
-    <main>
-      <h1>Helloo.. More interesting contents would be uploaded to this personal blog soon.</h1>
-    </main>
+      <main>
+        <h1>Helloo.. More interesting contents would be uploaded to this personal blog soon.</h1>
+      </main>
   );
 }
 
