7cd28e91469d0546fa4a766f3b580a4656ac6bb8
Sirisha Medicharla <sirishamedicharla@Sirishas-MacBook-Pro.local>
2025-03-22 11:17:20 -0700

    updated website content-v7

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
-
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
   return (
       <main>
-        <h1>Helloo.. More interesting contents would be uploaded to this personal blog soon.</h1>
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-          {todos.map((todo) => (
-              <li key={todo.id}>{todo.content}</li>
-          ))}
-        </ul>
+        <h1>Helloo.. More  contents would be uploaded to this personal blog soon.</h1>
+
       </main>
   );
 }
