12fed7029938ee151562f8a29f2336a3933bd3fb
Sirisha Medicharla <sirishamedicharla@Sirishas-MacBook-Pro.local>
2025-05-18 18:16:55 -0700

    updated website content-v13

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
+
+
 function App() {
     function funFactClick() {
         alert("No matter what happens in life, be good to people. Being good is wonderful legacy to leave behind..");
     }
   return (
       <main>
-              <button onClick={funFactClick}>Facts</button>
-
+              <button onClick={funFactClick}>Quote of the day</button>
       </main>
   );
 }
