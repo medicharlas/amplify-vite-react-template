c5435af504a53d50d409c2db3661756c4a5b6ce4
Sirisha Medicharla <sirishamedicharla@Sirishas-MacBook-Pro.local>
2025-11-23 15:02:45 -0800

    genAIWebAPP-InitialComit4

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 
-Amplify.configure(outputs);
+const apiUrl = "https://l93d45v2bl.execute-api.us-east-1.amazonaws.com/prod/";
+
+Amplify.configure(apiUrl);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
